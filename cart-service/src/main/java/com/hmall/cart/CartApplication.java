4ae4bd8fbf9ea9a22536a2539b512f16e378946f
lhd9000p <lanhedong@126.com>
2024-03-23 23:44:54 +0800

    抽取client接口到api模块

--- cart-service/src/main/java/com/hmall/cart/CartApplication.java
+++ cart-service/src/main/java/com/hmall/cart/CartApplication.java
@@ -7,9 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
-@EnableFeignClients
 @MapperScan("com.hmall.cart.mapper")
 @SpringBootApplication
+/* 当定义的FeignClient不在SpringBootApplication的扫描包范围时，这些FeignClient无法使用，需要添加basePackages或者clients */
+@EnableFeignClients(basePackages = "com.hmall.api.client")
 public class CartApplication {
     public static void main(String[] args) {
         SpringApplication.run(CartApplication.class, args);
